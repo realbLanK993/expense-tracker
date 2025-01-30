49407d6e31a39018986c136d163cbf8ee79ad027
bLanK <manishsuresh993@gmail.com>
2025-01-30 11:28:40 +0530

    Configured GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/expense-tracker", // Replace with your GitHub repository name
+  assetPrefix: "/expense-tracker/",
+};;
 
 export default nextConfig;
